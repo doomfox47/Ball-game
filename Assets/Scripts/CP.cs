ba3e3e16d5c1450b1ef01b4294070add6eb3fde0
Andrew Gauger <andygauge@gmail.com>
2021-07-15 17:44:05 -0700

    code

---
+++ Assets/Scripts/CP.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Transform transTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transTarget.position;
+    }
+}
---
+++ Assets/Scripts/RESET.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RESET : MonoBehaviour
+{
+    [SerializeField]
+    KeyCode keyRestart;
+
+    void Update()
+    {
+        if (Input.GetKey(keyRestart))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
